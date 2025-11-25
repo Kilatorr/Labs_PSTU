38b39da7b30ddb86c5f517c05e3cd65d79f2aff7
Romazanov <romazanov2007@gmail.com>
2025-11-25 14:14:06 +0500

    Add files via upload

---
+++ 1 sem/1.1/1.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b && a > c){
+        cout<<"a skebed";
+    }
+    if (b > a && b > c){
+        cout<<"b skebed";
+    }
+    if (c > b && c > b){
+        cout<<"c skebed";
+    }
+;
+;
+    return 0;
+}
